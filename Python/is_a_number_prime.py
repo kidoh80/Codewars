61390f187882be9772a6bb858811f33af481a17d
rvaldeabella <rvaldeabella@gmail.com>
2019-09-18 14:31:38 -0700

    add URL for prime exercise

--- Python/is_a_number_prime.py
+++ Python/is_a_number_prime.py
@@ -1,4 +1,6 @@
 /*
+https://www.codewars.com/kata/is-a-number-prime/python
+
 Define a function that takes an integer argument and returns logical value true or false depending on if the integer is a prime.
 
 Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
